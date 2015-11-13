da456ea5bac33a5bb3f973baf08cf38e871122e5
Le Nam <lenam.uit@gmail.com>
2015-11-13 15:07:47 +0700

    add skin

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -7,11 +7,4 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.contentsCss = 'css/font.css';
-	config.font_names = 'mitubachi Regular/mitubachi;' + config.font_names;
-	config.font_names = 'AhahaMojimoji Regular/AhahaMojimoji;' + config.font_names;
-	config.font_names = 'Pacifico Regular/Pacifico;' + config.font_names;
-	config.font_names = 'Frijole/Frijole;' + config.font_names;
-	config.font_names = 'ヒラギノ角ゴ Pro W3;' + config.font_names;
-	config.font_names = 'ヒラギノ角ゴ Pro W6;' + config.font_names;
 };
